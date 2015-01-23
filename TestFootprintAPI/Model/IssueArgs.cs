42dc2786d0bf2f628f690af9fa0b3caaf2ceac59
EricSun <mingmingruyuedlut@163.com>
2015-01-23 08:46:56 +0800

    first commit

---
+++ TestFootprintAPI/Model/IssueArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFootprintAPI.Model
+{
+    [System.Xml.Serialization.SoapTypeAttribute("ProjFields", "Interactive.Footprints")]
+    public class ProjFields
+    {
+        public string Custom__bField__bOne;
+        public string Custom__bField__bTwo;
+    }
+
+    [System.Xml.Serialization.SoapTypeAttribute("CreateIssueArgs", "Interactive.Footprints")]
+    public class IssueArgs
+    {
+        public string projectID;
+        public string title;
+        public string[] assignees;
+        public string priorityNumber;
+        public string status;
+        public string description;
+        public string selectContact;
+        public ProjFields projfields;
+    }
+}
---
+++ TestFootprintAPI/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestFootprintAPI.Manager;
+using TestFootprintAPI.Model;
+
+namespace TestFootprintAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Start to call footprint api:");
+                Console.WriteLine();
+
+                //Console.WriteLine("Call Create API:");
+                //IssueArgs issueArgs = GetIssueArgs();
+                //string issueId = CreateIssue(issueArgs);
+                //Console.WriteLine("Issue Id: {0}", issueId);
+                //Console.WriteLine("Call Create API Successfully!");
+                //Console.WriteLine();
+                //Console.WriteLine("Please press any key to test the next api.");
+                //Console.ReadKey();
+
+                Console.WriteLine("Call Search API:");
+                DateTime startTime = DateTime.Now;
+                string query = ConfigurationManager.AppSettings["FPSearchIssueQuery1"];
+                string issueResult = SearchIssue(query);
+                Console.WriteLine("Search Result:");
+                Console.WriteLine(issueResult);
+                Console.WriteLine(DateTime.Now - startTime);
+
+                //startTime = DateTime.Now;
+                //query = ConfigurationManager.AppSettings["FPSearchIssueQuery2"];
+                //issueResult = SearchIssue(query);
+                //Console.WriteLine("Search Result:");
+                //Console.WriteLine(issueResult);
+                //Console.WriteLine(DateTime.Now - startTime);
+
+                Console.WriteLine("Call Search API Successfully!");
+
+
+                Console.WriteLine("End. Call footprint api successfully!");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Call footprint api error!!!");
+                Console.WriteLine(ex);
+                Console.ReadKey();
+            }
+        }
+
+        public static string GetIssueDetail(int projectId, int issueId)
+        {
+            RetrieveIssueManager retrieveManager = new RetrieveIssueManager();
+            string username = ConfigurationManager.AppSettings["FPUserName"];
+            string psw = ConfigurationManager.AppSettings["FPPassword"];
+            string result = retrieveManager.MRWebServices__getIssueDetails(username, psw, "RETURN_MODE => 'xml'", projectId, issueId);
+            return result;
+        }
+
+        public static string CreateIssue(IssueArgs issueargs)
+        {
+            CreateIssueManager createIssuemanger = new CreateIssueManager();
+            string username = ConfigurationManager.AppSettings["FPUserName"];
+            string psw = ConfigurationManager.AppSettings["FPPassword"];
+            string IssueId = createIssuemanger.MRWebServices__createIssue(username, psw, "", issueargs);
+
+            return IssueId;
+        }
+
+        public static string SearchIssue(string query)
+        {
+            SearchDBManager searchManager = new SearchDBManager();
+            string username = ConfigurationManager.AppSettings["FPUserName"];
+            string psw = ConfigurationManager.AppSettings["FPPassword"];
+            string result = searchManager.MRWebServices__search(username, psw, "RETURN_MODE => 'xml'", query);
+            return result;
+        }
+
+        public static IssueArgs GetIssueArgs()
+        {
+            IssueArgs issue = new IssueArgs();
+            issue.title = "Test footprint service api";
+            issue.priorityNumber = "1";
+            issue.status = "Open";  //currrently only 'Open' works
+            issue.description = "This is test footprint service api";
+            issue.selectContact = "1";
+            issue.projectID = ConfigurationManager.AppSettings["FPCreateIssueProjectId"];
+            return issue;
+        }
+    }
+}
